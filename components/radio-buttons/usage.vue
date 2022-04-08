688a67e3a119b78768cb9b827fde21171f3ca84f
Orlando <omerayo87@gmail.com>
2022-04-08 18:44:57 -0500

    basic use of radio buttons

--- components/radio-buttons/usage.vue
+++ components/radio-buttons/usage.vue
@@ -24,19 +24,61 @@
       </template>
       <!-- Códígo ejemplo de un fichero completo -->
       <template #code>
-        <e-row>
-          <e-col col="24" md="12" lg="12" xl="12">
-            <h4>Códígo ejemplo de todo un fichero</h4>
-          </e-col>
-        </e-row>
+        <div>
+          <e-row no-gutters>
+            <e-col col="24">
+              <div v-prism>
+                <script type="prism-html-markup">
+                  <template>
+                    <e-container fluid>
+                      <e-row>
+                        <e-col col="24" md="12" lg="12" xl="12">
+                          <e-radio-group v-model="radioGroup">
+                              <e-radio
+                                                               v-for="n in 3"
+                                                               :key="n"
+                                                               :label="`Radio ${n}`"
+                                                               :input-value="n"
+                                                               >
+                              </e-radio>
+                          </e-radio-group>
+                        </e-col>
+                      </e-row>
+                    </e-container>
+                  </template>
+                </script>
+              </div>
+            </e-col>
+          </e-row>
+          <e-row no-gutters>
+            <e-col col="24">
+              <div v-prism="'language-javascript'">
+                <script type="prism-javascript">
+                      <script>
+                        import Vue from 'vue'
+                        export default Vue.extend({
+                          name: 'usage',
+                          data: () => ({
+                            radioGroup: 1,
+                          }),
+                        })
+                      <script>
+                </script>
+              </div>
+            </e-col>
+          </e-row>
+        </div>
       </template>
       <!-- Fragmento de códígo con componente y sus props en cuestión -->
       <template #example>
-        <e-row>
-          <e-col col="24" md="12" lg="12" xl="12">
-            <h4>Fragmento de códígo con componente y sus props en cuestión</h4>
-          </e-col>
-        </e-row>
+          <div v-prism>
+              <script type="prism-html-markup">
+                <e-radio
+                                        color=info
+                                 >
+                </e-radio>
+              </script>
+          </div>
       </template>
     </app-example-box>
   </e-container>
